99077cc6b947a2809c0a6ea5bf5e89005b4af7df
Saiful264 <saifulislamx264@gmail.com>
2023-02-18 17:19:06 +0600

    Triangle Area calculate code is write

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log('hello');
+function getInputValue(elementId){
+    const elementField = document.getElementById(elementId);
+    const elementFieldString = elementField.value;
+    const element = parseFloat(elementFieldString);
+    return element;
+}
+
+// Triangle Area code
+function triangleArea(b, h){
+    const triangleArea = 0.5 * b * h;
+    console.log(triangleArea);
+}
+document.getElementById("tri-calculate").addEventListener('click', function() {
+    // get the value
+    const b = getInputValue('tri-base');
+    const h = getInputValue('tri-height');
+    // get Triangle Area
+    triangleArea(b, h);
+    // set the value
+
+})
+
+
+function rectangleArea(w, l) {
+    const rectangleArea = w * l;
+    console.log(rectangleArea);
+    return rectangleArea;
+}
+
+document.getElementById('rectangle-calculate').addEventListener('click', function(){
+    const w = getInputValue('rectangle-width');
+    const l = getInputValue('rectangle-length');
+
+    rectangleArea(w, l);
+})
